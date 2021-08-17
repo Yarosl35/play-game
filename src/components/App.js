b221be6f6eba8bb1b6dbd2a737cee741280379fd
khitrov <khitrov.dev@gmail.com>
2021-08-17 15:54:01 +0300

    added: react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import { LeaderBoard } from "./leaderBoard/index";
 import { Board } from "./layout/index";
 import { leaderData } from "./leaderBoard/data/leaderData";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Board>
-        <LeaderBoard arrayList={leaderData[0].list} />
-      </Board>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/leader-board">
+            <Board>
+              <LeaderBoard arrayList={leaderData[0].list} />
+            </Board>
+          </Route>
+          <Route path="/">
+            <Board>
+              <LeaderBoard arrayList={leaderData[0].list} />
+            </Board>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
