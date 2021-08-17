74e022f204005f8a076c774ac4ce96af146ad2c5
khitrov <khitrov.dev@gmail.com>
2021-08-17 14:01:53 +0300

    added: menuList and change list leaderBoard

--- src/components/leaderBoard/data/leaderData.js
+++ src/components/leaderBoard/data/leaderData.js
@@ -2,21 +2,21 @@ export const leaderData = [
   {
     room: 1,
     list: [
-      { id: 1, name: "Matthew Choi", scope: 2000 },
-      { id: 2, name: "Stephen Yip", scope: 1000 },
-      { id: 3, name: "Edison Chan", scope: 893 },
-      { id: 4, name: "Francis Lo", scope: 567 },
-      { id: 5, name: "Jason cheung", scope: 500 },
+      { id: 1, name: "Matthew Choi", score: 5000 },
+      { id: 2, name: "Stephen Yip", score: 1000 },
+      { id: 3, name: "Edison Chan", score: 893 },
+      { id: 4, name: "Francis Lo", score: 7000 },
+      { id: 5, name: "Jason cheung", score: 500 },
     ],
   },
   {
     room: 2,
     list: [
-      { id: 1, name: "Matthew Choi", scope: 100 },
-      { id: 2, name: "Stephen Yip", scope: 400 },
-      { id: 3, name: "Edison Chan", scope: 600 },
-      { id: 4, name: "Francis Lo", scope: 200 },
-      { id: 5, name: "Jason cheung", scope: 500 },
+      { id: 1, name: "Matthew Choi", score: 100 },
+      { id: 2, name: "Stephen Yip", score: 400 },
+      { id: 3, name: "Edison Chan", score: 600 },
+      { id: 4, name: "Francis Lo", score: 200 },
+      { id: 5, name: "Jason cheung", score: 500 },
     ],
   },
 ];
