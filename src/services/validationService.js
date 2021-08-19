a78dd1d99b4b7f2373f3a096a7b44e2b8172f369
khitrov <khitrov.dev@gmail.com>
2021-08-19 18:41:58 +0300

    Added: resetPassword/User

--- src/services/validationService.js
+++ src/services/validationService.js
@@ -17,3 +17,18 @@ export const RegisterSchema = Yup.object().shape({
   ),
   schoolName: Yup.string().min(2, "should be 2 chars minimum."),
 });
+export const UserSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+  fullName: Yup.string()
+    .trim()
+    .matches(/^[a-zA-Z\s]*$/, "Is not in correct format")
+    .required("Required"),
+  schoolName: Yup.string().min(2, "should be 2 chars minimum."),
+  oldPassword: Yup.string().required("Required"),
+  newPassword: Yup.string()
+    .required("Required")
+    .min(8, "should be 8 chars minimum."),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("newPassword"), null], "Passwords must match")
+    .required("Required"),
+});
