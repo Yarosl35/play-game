45c111fc18777db406150808bce83a2978564ea6
khitrov <khitrov.dev@gmail.com>
2021-08-19 17:20:09 +0300

    added: room-list page

---
+++ src/components/pages/RoomList/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import styles from "./Rooms.module.css";
+import { playersData } from "./data/playersData";
+import { RoomsItem } from "./RoomsItem/index";
+
+export const RoomList = () => {
+  const listItems = playersData.map((data) => {
+    return <RoomsItem data={data} />;
+  });
+
+  return (
+    <div className={styles.containerFlex}>
+      <div className={styles.mainContainer}>
+        <div className={styles.containerBtn}>
+          <button className={styles.importBtn}>Create new room</button>
+        </div>
+        <div className={styles.containerScroll}>
+          <ul className={styles.roomList}>{listItems}</ul>
+        </div>
+      </div>
+    </div>
+  );
+};
