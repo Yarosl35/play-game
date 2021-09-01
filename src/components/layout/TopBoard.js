89601037a935b186204ded4af856c067f650f2eb
khitrov <khitrov.dev@gmail.com>
2021-09-01 18:11:35 +0300

    added: top menu

--- src/components/layout/TopBoard.js
+++ src/components/layout/TopBoard.js
@@ -1,10 +1,17 @@
 import styles from "./layout.module.css";
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router";
 import Logo from "./logo.svg";
 import LogoUser from "./logoUser.svg";
 import { Link } from "react-router-dom";
 
 export const TopBoard = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const history = useHistory();
+  const selectProfile = () => {
+    setShowMenu(false);
+    history.push("/user");
+  };
   return (
     <div className={styles.topBar}>
       <Link
@@ -14,7 +21,20 @@ export const TopBoard = () => {
         <img className={styles.logo} src={Logo} alt="logo" />
       </Link>
       <div className={styles.containerMenuBoard}>
-        <img className={styles.logoUser} src={LogoUser} alt="Logo user" />
+        <div
+          onClick={() => {
+            setShowMenu((stateShow) => !stateShow);
+          }}
+          className={styles.topBarMenu}
+        >
+          <img className={styles.logoUser} src={LogoUser} alt="Logo user" />
+        </div>
+        {showMenu ? (
+          <ul className={styles.menuList}>
+            <li onClick={selectProfile}>Profile</li>
+            <li>Log out</li>
+          </ul>
+        ) : null}
       </div>
     </div>
   );
