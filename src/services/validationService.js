ffcf1ec2d295dd2076a900f38e74e4de8390a0d7
khitrov <khitrov.dev@gmail.com>
2021-09-12 00:24:12 +0800

    Updated password regex

--- src/services/validationService.js
+++ src/services/validationService.js
@@ -4,13 +4,13 @@ export const SignupSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email").required("Required"),
   password: Yup.string()
     .required("Required")
-    .min(8, "should be 8 chars minimum."),
+    .matches("(?=.*[a-z])(?=.*[0-9])(?=.{8,24})", "Password must be formed with 8 characters including big and small letters and numbers")
 });
 export const RegisterSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email").required("Required"),
   password: Yup.string()
     .required("Required")
-    .min(8, "should be 8 chars minimum."),
+    .matches("(?=.*[a-z])(?=.*[0-9])(?=.{8,24})", "Password must be formed with 8 characters including big and small letters and numbers"),
   confirmPassword: Yup.string().oneOf(
     [Yup.ref("password"), null],
     "Passwords must match"
@@ -29,8 +29,8 @@ export const UserSchema = Yup.object().shape({
     .matches(/^[a-zA-Z\s]*$/, "Is not in correct format")
     .required("Required"),
   schoolName: Yup.string().min(2, "should be 2 chars minimum."),
-  oldPassword: Yup.string().min(8, "should be 8 chars minimum."),
-  newPassword: Yup.string().min(8, "should be 8 chars minimum."),
+  oldPassword: Yup.string().matches("(?=.*[a-z])(?=.*[0-9])(?=.{8,24})", "Password must be formed with 8 characters including big and small letters and numbers"),
+  newPassword: Yup.string().matches("(?=.*[a-z])(?=.*[0-9])(?=.{8,24})", "Password must be formed with 8 characters including big and small letters and numbers"),
   confirmPassword: Yup.string().oneOf(
     [Yup.ref("newPassword"), null],
     "Passwords must match"
@@ -40,7 +40,7 @@ export const UserSchema = Yup.object().shape({
 export const newPasswordSchema = Yup.object().shape({
   newPassword: Yup.string()
     .required("Required")
-    .min(8, "should be 8 chars minimum."),
+    .matches("(?=.*[a-z])(?=.*[0-9])(?=.{8,24})", "Password must be formed with 8 characters including big and small letters and numbers"),
   confirmPassword: Yup.string().oneOf(
     [Yup.ref("newPassword"), null],
     "Passwords must match"
