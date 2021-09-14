7a720f01d3e1268ef9d49b958f49f37278509e2d
khitrov <khitrov.dev@gmail.com>
2021-09-14 11:44:59 +0900

    Add socket token

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
+
 export const socket = io(process.env.REACT_APP_WS_URL, {
   auth: {
-    // token: `accountToken: ${cookies.get("userToken").token}`,
-    token: `accountToken: test`,
-  },
+    token: `accountToken: ${ (cookies.get("userToken") || {}).token }`,
+  }
 });
