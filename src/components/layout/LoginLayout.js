414f1edac4897ba3665721fbdbadd97d8f99701e
khitrov <khitrov.dev@gmail.com>
2021-09-21 11:58:12 +0300

    redirect to room-list

--- src/components/layout/LoginLayout.js
+++ src/components/layout/LoginLayout.js
@@ -3,10 +3,12 @@ import styles from "./layout.module.css";
 import { TopBar } from "./TopBar";
 import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { useSelector } from "react-redux";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 export const LoginLayout = ({ children }) => {
-  const auth = useSelector(({ auth }) => auth);
+  const auth = cookies.get("roomID");
   const history = useHistory();
   useEffect(() => {
     if (auth) return history.push("/room-list");
