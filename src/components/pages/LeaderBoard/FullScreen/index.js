e2f61d6993b97459ed2ba6a335d50a2f02b1e0ac
khitrov <khitrov.dev@gmail.com>
2021-09-21 11:40:07 +0300

    fixed style/full-screen

--- src/components/pages/LeaderBoard/FullScreen/index.js
+++ src/components/pages/LeaderBoard/FullScreen/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { ListLeader } from "./../listLeader/index";
-import { SelectList } from "../../../queries/SelectList";
 import { useSelector } from "react-redux";
 
 import { Panel } from "./../../../layout/Panel";
@@ -10,8 +9,6 @@ import styles from "./FullScreen.module.css";
 export const FullScreen = () => {
   const leaderBoard = useSelector(({ leaderBoard }) => leaderBoard);
   const roomSelect = useSelector(({ roomSelect }) => roomSelect);
-  const listRooms = useSelector(({ listRooms }) => listRooms);
-  const [roomSelection, setRoomSelection] = useState([]);
   const user = useSelector(({ user }) => user);
   const [leaderListRoom, setLeaderListRoom] = useState([]);
 
@@ -22,20 +19,6 @@ export const FullScreen = () => {
     },
     [leaderBoard]
   );
-
-  useEffect(() => {
-    const roomSelectionMap = listRooms
-      ? listRooms.map((room) => {
-          return {
-            id: room.roomID,
-            name: room.name,
-            value: room.roomID,
-          };
-        })
-      : [];
-    setRoomSelection(roomSelectionMap);
-    changeRoom(roomSelect.roomID);
-  }, [listRooms, changeRoom, roomSelect.roomID]);
 
   useEffect(() => {
     changeRoom(roomSelect.roomID);
